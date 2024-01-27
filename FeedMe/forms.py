ca1c3ffde1ee4bd4903d158c4e6368347e17594d
JellyFishJuggler <146509602+JellyFishJuggler@users.noreply.github.com>
2024-01-27 18:50:17 +0530

    minor changes

--- FeedMe/forms.py
+++
@@ -1,8 +0,0 @@
-# feedme/forms.py
-from django import forms
-from .models import UserProfile
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['username', 'password']

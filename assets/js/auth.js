0c2f7af1a434c37b807119122281721c4f406c2a
JellyFishJuggler <146509602+JellyFishJuggler@users.noreply.github.com>
2024-02-10 23:20:30 +0530

    feat: auth complete

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,10 @@
 import {
   signInWithPopup,
   GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { auth } from "./firebase.js";
 
@@ -9,41 +13,116 @@ const provider = new GoogleAuthProvider();
 export const signInWithGoogle = () => {
   signInWithPopup(auth, provider)
     .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      alert("Sign in with Google successful", user);
+      window.location.replace("/");
     })
     .catch((error) => {
-      console.log(error);
+      alert(error.message);
     });
 };
 
-export const isLoggedIn = async () => {
-  const ans = await auth.onAuthStateChanged((user) => {
-    if (user) {
-      return true;
-    } else {
-      return false;
-    }
+export const registerWithEmail = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.replace("/");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+export const signInWithEmail = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.replace("/");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+export const isLoggedIn = () => {
+  return localStorage.getItem("userLoggedIn") === "true";
+};
+
+export const logOut = () => {
+  signOut(auth).then(() => {
+    alert("Logged out");
+    // refresh the page
+    window.location.replace("/");
   });
-  return ans;
 };
 
 if (window.location.pathname === "/register.html") {
   const signInButton = document.getElementById("signInWithGoogleButton");
   signInButton.addEventListener("click", signInWithGoogle);
+
+  const form = document.getElementById("registerForm");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (form.checkValidity() === false) {
+      form.reportValidity();
+      return;
+    }
+    // FormData
+    const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    } else {
+      registerWithEmail(email, password);
+    }
+  });
+} else if (window.location.pathname === "/login.html") {
+  const signInButton = document.getElementById("signInWithGoogleButton");
+  signInButton.addEventListener("click", signInWithGoogle);
+
+  const form = document.getElementById("loginForm");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (form.checkValidity() === false) {
+      form.reportValidity();
+      return;
+    }
+    // FormData
+    const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    signInWithEmail(email, password);
+  });
 }
+
+const handleHomePage = () => {
+  document.querySelector("#registerButton").style.display = "none";
+  const logOutButton = document.getElementById("logOutButton");
+  logOutButton.style.display = "block";
+  logOutButton.addEventListener("click", logOut);
+};
 
 document.body.onload = () => {
   if (isLoggedIn()) {
     switch (window.location.pathname) {
       case "/":
-        document.querySelector("#registerButton").style.display = "none";
+        handleHomePage();
         break;
+      case "/login.html":
       case "/register.html":
         window.location.replace("/");
         break;
     }
   }
+
+  // Add a listener for the auth state
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // set localStorage
+      localStorage.setItem("userLoggedIn", true);
+    } else {
+      // remove localStorage
+      localStorage.removeItem("userLoggedIn");
+    }
+  });
 };
